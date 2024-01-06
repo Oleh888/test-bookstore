593213b3d194197479b38c485719d10853d65e98
Oleh Kuzmenko <okuzmenko@n-ix.com>
2024-01-06 23:17:06 +0200

    added graphql mutation

--- src/main/java/com/book/store/graphql/GraphqlBookController.java
+++ src/main/java/com/book/store/graphql/GraphqlBookController.java
@@ -6,6 +6,7 @@ import java.math.BigDecimal;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -24,6 +25,22 @@ public class GraphqlBookController {
   @QueryMapping
   public List<Book> bookList() {
     return bookMapper.toBooksResponse(bookService.getAllBooks());
+  }
+
+  @MutationMapping
+  public void addBook(@Argument String author, @Argument String title, @Argument Integer publicationYear, @Argument Double price) {
+    bookService.saveBook(new BookService.BookDto(null, title, author, BigDecimal.valueOf(price), publicationYear));
+  }
+
+  @MutationMapping
+  public void updateBook(@Argument String id, @Argument String author, @Argument String title,
+                         @Argument Integer publicationYear, @Argument Double price) {
+    bookService.updateBook(new BookService.BookDto(null, title, author, BigDecimal.valueOf(price), publicationYear), id);
+  }
+
+  @MutationMapping
+  public void deleteBook(@Argument String id) {
+    bookService.deleteBook(id);
   }
 
   public record Book(String id, String title, String author, BigDecimal price, Integer publicationYear) {
